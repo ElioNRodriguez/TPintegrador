32c9850c2890e1161cb568e5b09bb333a345d7a0
miguedorta7 <75344292+miguedorta7@users.noreply.github.com>
2023-11-24 21:00:48 -0300

    se agregan estilos e iconos de botones

--- src/components/TaskItemComponent/TaskItem.jsx
+++ src/components/TaskItemComponent/TaskItem.jsx
@@ -1,48 +1,65 @@
-import React from 'react';
-import { useState } from 'react';
-
+import React from "react";
+import { useState } from "react";
+import "../TaskItemComponent/TaskItem.css";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegCheckSquare } from "react-icons/fa";
+import { FaRegEdit } from "react-icons/fa";
+import { TbEditOff } from "react-icons/tb";
 
 const TaskItem = ({ task, onCompleteTask, onDeleteTask, onEditTask }) => {
+  const { id, name, completed } = task;
+  const [editing, setEditing] = useState(false);
+  const [editedName, setEditedName] = useState(name);
 
-    const { id, name, completed } = task;
-    const [editing, setEditing] = useState(false);
-    const [editedName, setEditedName] = useState(name);
+  const handleComplete = () => {
+    onCompleteTask(id);
+  };
 
-    const handleComplete = () => {
-        onCompleteTask(id)
-    };
+  const handleDelete = () => {
+    onDeleteTask(id);
+  };
 
-    const handleDelete = () => {
-        onDeleteTask(id)
-    };
+  const handleEdit = () => {
+    if (editing) {
+      onEditTask(id, editedName);
+      setEditing(false);
+    } else {
+      setEditing(true);
+    }
+  };
 
-    const handleEdit = () => {
-        if (editing) {
-            onEditTask(id, editedName);
-            setEditing(false);
-        } else {
-            setEditing(true);
-        }
-    };
+  const handleInputChange = (e) => {
+    setEditedName(e.target.value);
+  };
 
-    const handleInputChange = e => {
-        setEditedName(e.target.value);
-    };
-
-
-    return (
-        // si esta editando habilita input si no solo muestra la tarea
-        <div>
-            {editing ? (
-                <input type="text" value={editedName} onChange={handleInputChange} />
-            ) : (
-                <span style={{ textDecoration: completed ? 'line-through' : 'none' }}>{name}</span>
-            )}
-            <button onClick={handleComplete}>Completar</button>
-            <button onClick={handleDelete}>Eliminar</button>
-            <button onClick={handleEdit}>{editing ? 'Guardar' : 'Editar'}</button>
-        </div>
-    );
-}
+  return (
+    // si esta editando habilita input si no solo muestra la tarea
+    <div className="item-container">
+      <div className="container-inputlist">
+        {editing ? (
+          <input
+            type="text"
+            value={editedName}
+            onChange={handleInputChange}
+            className="input-edit"
+          />
+        ) : (
+          <span
+            style={{ textDecoration: completed ? "line-through" : "none" }}
+            className="span-line"
+          >
+            {name}
+          </span>
+        )}
+      </div>
+      
+      <div className="container-buttons">
+        <button onClick={handleComplete} className="btn-tareas btn-completar"><FaRegCheckSquare/></button>
+        <button onClick={handleDelete} className="btn-tareas btn-eliminar"><FaRegTrashAlt /></button>
+        <button onClick={handleEdit} className="btn-tareas btn-guardar">{editing ? <TbEditOff/> : <FaRegEdit/>}</button>
+      </div>
+    </div>
+  );
+};
 
 export default TaskItem;
